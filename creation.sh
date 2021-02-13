d2a5d457a417b8629b5b1899b0fe98a2f03e6407
Alex <aslexag0@gmail.com>
2021-02-13 14:33:15 +0100

    Example1

--- creation.sh
+++ creation.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-xterm -e sudo docker run --name pc -it --network b1 metabit1000/debian &
+xterm -e docker run --name pc -it --rm --network b1 metabit1000/debian &
 
-sudo docker run --name router1 -d --network b1 metabit1000/openwrtimage
-sudo docker network connect b2 router1
+docker run --name router1 -d --rm --network b1 metabit1000/openwrtimage
+docker network connect b2 router1
 
-sudo docker run --name Internet -d --network b2 metabit1000/apacheserver
+docker run --name Internet -d --rm --network b2 metabit1000/apacheserver
 
-sudo docker ps
+docker ps
 
-gnome-terminal -- bash -c "sudo docker network inspect b1; exec bash"
-gnome-terminal -- bash -c "sudo docker network inspect b2; exec bash"
+gnome-terminal -- bash -c "docker network inspect b1; exec bash"
+gnome-terminal -- bash -c "docker network inspect b2; exec bash"
 
---
+++ stopContainers.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+docker stop pc
+#docker rm pc
+
+docker stop router1
+#docker rm router1
+
+docker stop Internet
+#docker rm Internet
+
